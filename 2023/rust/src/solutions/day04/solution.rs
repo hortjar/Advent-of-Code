461efae1bf1744a9f181e04a529e193c76a64b0e
Jaroslav Hort <hortjaroslav0@gmail.com>
2023-12-04 14:56:11 +0100

    2023: day04 wip2

--- 2023/rust/src/solutions/day04/solution.rs
+++ 2023/rust/src/solutions/day04/solution.rs
@@ -1,72 +1,54 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 use crate::solutions::AoCDay;
 
 pub struct Day04;
 
-impl Day04 {}
+impl Day04 {
+    fn parse_numbers(&self, input: &str) -> Vec<u32> {
+        return input
+            .trim()
+            .split(" ")
+            .filter(|x| !x.is_empty())
+            .map(|num| num.trim().parse::<u32>().unwrap_or_default())
+            .collect::<Vec<u32>>();
+    }
+    fn parse_cards(&self, input: &str) -> Vec<Card> {
+        return input
+            .lines()
+            .map(|line| {
+                let l = line.replace("Card ", "");
+                let split = l.split(|c| c == ':' || c == '|').collect::<Vec<&str>>();
+
+                let card = &split
+                    .get(0)
+                    .expect("No card on index 0")
+                    .trim()
+                    .parse::<u32>()
+                    .unwrap_or_default();
+
+                let winning_nums =
+                    self.parse_numbers(&split.get(1).expect("Mo numbers on index 1"));
+                let drawn_nums = self.parse_numbers(&split.get(2).expect("Mo numbers on index 2"));
+
+                return Card {
+                    card: *card,
+                    winning_numbers: winning_nums.to_owned(),
+                    drawn_numbers: drawn_nums.to_owned(),
+                };
+            })
+            .collect::<Vec<Card>>();
+    }
+}
 
 impl AoCDay for Day04 {
     fn part01(&self) -> String {
         let input = self.read_input(4, 1);
 
-        let sum = input
-            .lines()
-            .map(|line| {
-                let l = line.replace("Game ", "");
-                let mut split = l.split(|c| c == ':' || c == '|');
-
-                let game = &split
-                    .to_owned()
-                    .nth(0)
-                    .unwrap_or_default()
-                    .parse::<u32>()
-                    .unwrap_or_default();
-
-                let winning_nums = &split
-                    .nth(1)
-                    .unwrap_or_default()
-                    .trim()
-                    .split(" ")
-                    .map(|num| num.trim().parse::<u32>().unwrap_or_default())
-                    .collect::<Vec<u32>>();
-
-                let drawn_nums = &split
-                    .nth(0)
-                    .unwrap_or_default()
-                    .trim()
-                    .split(" ")
-                    .map(|num| num.trim().parse::<u32>().unwrap_or_default())
-                    .collect::<Vec<u32>>();
-
-                println!("Winning:");
-                winning_nums.iter().for_each(|x| print!("{}, ", x));
-                println!("");
-
-                println!("Drawn:");
-                drawn_nums.iter().for_each(|x| print!("{}, ", x));
-                println!("");
-
-                return Card {
-                    game: *game,
-                    winning_numbers: winning_nums.to_owned(),
-                    drawn_numbers: drawn_nums.to_owned(),
-                };
-            })
-            .map(|card| {
-                return card
-                    .drawn_numbers
-                    .iter()
-                    .filter(|x| card.winning_numbers.contains(x))
-                    .map(|f| f.to_owned())
-                    .collect::<HashSet<u32>>();
-            })
-            .map(|card| {
-                let mut score = if card.len() > 0 { 1 } else { 0 };
-                card.iter().skip(1).for_each(|_| score *= 2);
-                println!("score {} card len {}", score, card.len());
-                return score;
-            })
+        let sum = self
+            .parse_cards(&input)
+            .iter()
+            .map(|card| card.score())
             .sum::<u32>();
 
         return sum.to_string();
@@ -76,12 +58,40 @@ impl AoCDay for Day04 {
         let input = self.read_input(4, 2);
         let mut sum = 0;
 
+        let parsed = self.parse_cards(&input);
+        let cards = parsed.iter().map(|card| {
+            let size = card.matches().len();
+            let mut hm: HashMap<u32, Vec<usize>> = HashMap::new();
+            hm.insert(card.card, vec![size]);
+            return hm;
+        });
+
+        for card_map in cards {}
+
         return sum.to_string();
     }
 }
 
 struct Card {
-    game: u32,
+    card: u32,
     winning_numbers: Vec<u32>,
     drawn_numbers: Vec<u32>,
+}
+
+impl Card {
+    fn matches(&self) -> Vec<u32> {
+        return self
+            .drawn_numbers
+            .iter()
+            .filter(|x| self.winning_numbers.contains(x))
+            .map(|f| f.to_owned())
+            .collect::<Vec<u32>>();
+    }
+
+    fn score(&self) -> u32 {
+        let _matches = self.matches();
+        let mut score = if _matches.len() > 0 { 1 } else { 0 };
+        _matches.iter().skip(1).for_each(|_| score *= 2);
+        return score;
+    }
 }
