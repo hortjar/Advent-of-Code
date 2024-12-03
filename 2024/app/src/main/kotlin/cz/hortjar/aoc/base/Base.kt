2cf95116b95b11390d37bd3215f44ea193be366c
Jaroslav Hort <hortjaroslav0@gmail.com>
2024-12-03 07:22:55 +0100

    2024: day02 part02

--- 2024/app/src/main/kotlin/cz/hortjar/aoc/base/Base.kt
+++ 2024/app/src/main/kotlin/cz/hortjar/aoc/base/Base.kt
@@ -3,14 +3,15 @@ package cz.hortjar.aoc.base
 import java.io.File
 import java.nio.file.Paths
 
-abstract class Base(day: Int) {
+abstract class Base(day: Int, debug: Boolean = false) {
   var input: List<String> = emptyList()
 
   init {
-    var paddedDay = "$day".padStart(2, '0')
-    var path = Paths.get("").toAbsolutePath().toString()
+    val paddedDay = "$day".padStart(2, '0')
+    val path = Paths.get("").toAbsolutePath().toString()
+    val suffix = if (debug) "_test" else ""
 
-    input = File("$path/app/src/main/kotlin/cz/hortjar/aoc/day$paddedDay/input").readLines()
+    input = File("$path/app/src/main/kotlin/cz/hortjar/aoc/day$paddedDay/input$suffix").readLines()
   }
 
   abstract fun solve01(): Int
--- 2024/app/src/main/kotlin/cz/hortjar/aoc/day02/Day02.kt
+++ 2024/app/src/main/kotlin/cz/hortjar/aoc/day02/Day02.kt
@@ -1,33 +1,55 @@
 package cz.hortjar.aoc.day02
 
 import cz.hortjar.aoc.base.Base
-import kotlin.math.abs
 
 class Day02 : Base(2) {
-  override fun solve01(): Int {
-    val safeLines = this.input.fold(0) { acc, line ->
-      val splitLine = line.split(" ")
-      var lineArray: List<Int> = emptyList()
-      for (i in 0..<splitLine.size - 1) {
-        val num = splitLine[i].toInt()
-        val nextNum = splitLine[i + 1].toInt()
-
-        lineArray += (num - nextNum)
+  private fun getNumbers(): List<List<Int>> {
+    return this.input.map { item ->
+      item.split(" ").map { number ->
+        number.toInt()
       }
+    }
+  }
 
-      val hasIllegalDifference = lineArray.count { num -> num > 3 || num < -3 || num == 0 } > 0
-      val hasPositive = lineArray.count { num -> num > 0 } > 0
-      val hasNegative = lineArray.count { num -> num < 0 } > 0
+  private fun generateMissingCombinations(input: List<Int>): List<List<Int>> {
+    return input.indices.map { index ->
+      input.filterIndexed { i, _ -> i != index }
+    }
+  }
 
-      val res = !hasIllegalDifference && hasPositive xor hasNegative
-      
-      if (res) acc + 1 else acc
+  private fun isLineSafe(line: List<Int>): Boolean {
+    var lineDiff: List<Int> = emptyList()
+    for (i in 0..<line.size - 1) {
+      val num = line[i]
+      val nextNum = line[i + 1]
+
+      lineDiff += (num - nextNum)
+    }
+
+    val hasIllegalDifference = lineDiff.count { num -> num > 3 || num < -3 || num == 0 } > 0
+    val hasPositive = lineDiff.count { num -> num > 0 } > 0
+    val hasNegative = lineDiff.count { num -> num < 0 } > 0
+
+    return !hasIllegalDifference && hasPositive xor hasNegative
+  }
+
+  override fun solve01(): Int {
+    val safeLines = getNumbers().fold(0) { acc, line ->
+      if (isLineSafe(line)) acc + 1 else acc
     }
 
     return safeLines
   }
 
   override fun solve02(): Int {
-    return 0
+    val safeLines = getNumbers().fold(0) { acc, line ->
+      val combinations = generateMissingCombinations(line)
+      val safe = combinations.fold(0) { combinationAcc, combination ->
+        if (isLineSafe(combination)) combinationAcc + 1 else combinationAcc
+      }
+      if (safe > 0) acc + 1 else acc
+    }
+
+    return safeLines
   }
 }
